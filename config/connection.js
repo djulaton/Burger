726a0b2e71632db263cf23afeca88e71e5b89b52
Diana <dianajulaton@gmail.com>
2018-08-25 14:26:43 -0700

    final test

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,13 @@
 // require mysql
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "mateo",
-  database: "burgers_db"
-});
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "mateo",
+//   database: "burgers_db"
+// });
 
 // Initiate MySQL Connection.
 if (process.env.JAWSDB_URL) {
